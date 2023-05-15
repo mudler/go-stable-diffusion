28b610f64a123438aa67afc242dc8622655bdc50
mudler <mudler@mocaccino.org>
2023-05-15 14:30:30 +0200

    fix

--- stablediffusion.cpp
+++ stablediffusion.cpp
@@ -24,55 +24,6 @@ using namespace std;
 int generate_image(int height, int width, int mode, int  step, int seed, const char *positive_prompt, const char *negative_prompt, const char *dst)
 {
 
-    /*
-	int height, width, mode, step, seed;
-	string positive_prompt, negative_prompt;
-
-	// default setting
-	height = 256;
-	width = 256;
-	mode = 0;
-	step = 15;
-	seed = 42;
-	positive_prompt = "floating hair, portrait, ((loli)), ((one girl)), cute face, hidden hands, asymmetrical bangs, beautiful detailed eyes, eye shadow, hair ornament, ribbons, bowties, buttons, pleated skirt, (((masterpiece))), ((best quality)), colorful";
-	negative_prompt = "((part of the head)), ((((mutated hands and fingers)))), deformed, blurry, bad anatomy, disfigured, poorly drawn face, mutation, mutated, extra limb, ugly, poorly drawn hands, missing limb, blurry, floating limbs, disconnected limbs, malformed hands, blur, out of focus, long neck, long body, Octane renderer, lowres, bad anatomy, bad hands, text";
-
-	*/
-
-    // parse the magic.txt
-	ifstream magic;
-	magic.open("magic.txt");
-	if (!magic) {
-		cout << "can not find magic.txt, using the default setting" << endl;
-	}
-	else {
-		string content = "";
-		int i = 0;
-		for (i = 0; i < 7; i++) {
-			if (getline(magic, content)) {
-				switch (i)
-				{
-				case 0:height = stoi(content);
-				case 1:width = stoi(content);
-				case 2:mode = stoi(content);
-				case 3:step = stoi(content);
-				case 4:seed = stoi(content);
-				case 5:positive_prompt = content.c_str();
-				case 6:negative_prompt = content.c_str();
-				default:break;
-				}
-			}
-			else {
-				break;
-			}
-		}
-		if (i != 7) {
-			cout << "magic.txt has wrong format, please fix it" << endl;
-			return 0;
-		}
-
-	}
-
 	if (seed == 0) {
 		seed = (unsigned)time(NULL);
 	}
