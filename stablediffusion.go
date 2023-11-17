902db5f066fd137697e3b69d0fa10d4782bd2c2f
Ettore Di Giacinto <mudler@users.noreply.github.com>
2023-11-17 16:55:36 +0100

    Update stablediffusion.go

--- stablediffusion.go
+++ stablediffusion.go
@@ -1,7 +1,7 @@
 package stablediffusion
 
-// #cgo CXXFLAGS: -I./ -I./ncnn/src -I./ncnn -I./ncnn/build/src/ -I./stable-diffusion/x86/vs2019_opencv-mobile_ncnn-dll_demo/vs2019_opencv-mobile_ncnn-dll_demo/ -std=c++17
-// #cgo LDFLAGS: -L./ -lstablediffusion -lgomp -lopencv_core -lopencv_imgcodecs -lm -lstdc++
+// #cgo CXXFLAGS: -I${SRCDIR}/ -I${SRCDIR}/ncnn/src -I${SRCDIR}/ncnn -I${SRCDIR}/ncnn/build/src/ -I${SRCDIR}/stable-diffusion/x86/vs2019_opencv-mobile_ncnn-dll_demo/vs2019_opencv-mobile_ncnn-dll_demo/ -std=c++17
+// #cgo LDFLAGS: -L${SRCDIR}/ -lstablediffusion -lgomp -lopencv_core -lopencv_imgcodecs -lm -lstdc++
 // #include "stablediffusion.h"
 // #include <stdlib.h>
 import "C"
